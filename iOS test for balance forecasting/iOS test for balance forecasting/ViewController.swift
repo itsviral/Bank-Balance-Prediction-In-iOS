4a20fe577411c69789b6bf71db370301bf28c942
viral <its_viral@outlook.com>
2018-10-11 03:19:39 +0100

    predict value in view controller

--- iOS test for balance forecasting/iOS test for balance forecasting/ViewController.swift
+++ iOS test for balance forecasting/iOS test for balance forecasting/ViewController.swift
@@ -9,12 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let model = MyModel()
+        
+        guard let result = try? model.prediction(day: 26, month: 8) else {
+            fatalError("Prediction failed!")
+        }
+        
+        print(result.balance)
+        
+        var counterDays : Double = 27
+        var months : Double = 8
+        
+        for _ in 1...28{
+            
+            guard let result = try? model.prediction(day: counterDays , month : months) else {
+                fatalError("Prediction failed!")
+            }
+            
+            counterDays = counterDays + 1
+            if counterDays == 31{
+                counterDays = 1
+    
+                months = months + 1
+                
+                if months == 13{
+                    months = 1
+                }
+            }
+            print(result.balance)
+        }
+        
     }
-
-
+    
+    
 }
 
